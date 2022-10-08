8342fb2725ed029bf32918346041ca8fd5a5628f
Antoine T-S <anto291100@gmail.com>
2022-10-07 21:56:04 -0400

    sniper started

---
+++ Assets/Scripts/Weapons/Sniper.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : Weapon
+{
+    public Sniper() : base(50,0,10,2,Vector2.zero,null,null)
+    {
+        
+    }
+
+    override public void Shoot()
+    {
+       
+    }
+}
