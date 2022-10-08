24ebb691586bff172806ac73d997d82a251d3dab
Antoine T-S <anto291100@gmail.com>
2022-10-08 13:37:24 -0400

    fix popuptext + balance

--- Assets/Scripts/GameProjectiles/SMGBullet.cs
+++ Assets/Scripts/GameProjectiles/SMGBullet.cs
@@ -6,6 +6,6 @@ public class SMGBullet : GameProjectile
 {
     public void Init(Weapon parent, Vector2 shootDirection)
     {
-        base.Init(1, 1f, 5, parent, shootDirection);
+        base.Init(1, 0.2f, 10, parent, shootDirection);
     }
 }
